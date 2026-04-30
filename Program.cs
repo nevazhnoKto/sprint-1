0ee135d9a7d37005e6d90916c95b61f4ceccab54
Тамакулов Иван Сергеевич <ivan.tamakulov@russianpost.ru>
2026-04-30 15:11:17 +0400

    Добавил EnsureCreated.

--- Program.cs
+++ Program.cs
@@ -64,6 +64,12 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+using (var scope = app.Services.CreateScope())
+{
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	db.Database.EnsureCreated();
+}
+
 app.MapControllers();
 
 app.Run();
