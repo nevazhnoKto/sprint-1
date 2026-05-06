b8abf96b90f1405546da206faa5c4c21bbaf971f
IvanTamakulov <itamakulov@gmail.com>
2026-05-06 14:52:55 +0300

    2. Репозитории уже были созданы. 3. Добавил миграцию.

--- WebApiTamakulov/Program.cs
+++ WebApiTamakulov/Program.cs
@@ -65,9 +65,9 @@ app.UseAuthorization();
 
 using (var scope = app.Services.CreateScope())
 {
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
-} 
+	var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+	db.Database.Migrate();
+}
 
 app.MapControllers();
 
