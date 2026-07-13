2fc9dd38e332ec6a772d83914e43570055d0d246
Тамакулов Иван Сергеевич <ivan.tamakulov@russianpost.ru>
2026-07-13 16:17:25 +0400

    10. Добавил реальзицию RedisService.

--- Event/Event.Application/Interfaces/IRedisService.cs
+++ Event/Event.Application/Interfaces/IRedisService.cs
@@ -7,6 +7,26 @@ namespace Event.Application.Interfaces
 	/// </summary>
 	public interface IRedisService
 	{
-		
+		/// <summary>
+		/// Получить данные по eventId.
+		/// </summary>
+		/// <param name="eventId">ИД события.</param>
+		/// <returns>Модель события.</returns>
+		Task<EventModel?> GetCacheForIdAsync(Guid eventId);
+
+		/// <summary>
+		/// Записать данные для события.
+		/// </summary>
+		/// <param name="eventId">ИД события.</param>
+		/// <param name="value">Модель события.</param>
+		/// <returns>Boolean</returns>
+		Task<bool> SetCacheAsync(Guid eventId, EventModel value);
+
+		/// <summary>
+		/// Удалить информуцию события из кеша.
+		/// </summary>
+		/// <param name="eventId">ИД события.</param>
+		/// <returns>Boolean</returns>
+		Task<bool> DeleteCacheAsync(Guid eventId);
 	}
 }
--- Event/Event.Infrastructure/Service/RedisService.cs
+++ Event/Event.Infrastructure/Service/RedisService.cs
@@ -1,13 +1,112 @@
 ﻿using Event.Application.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Event.Domain.Models;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System.Text.Json;
 
 namespace Event.Infrastructure.Service
 {
-	public class RedisService: IRedisService
+#pragma warning disable CS1591
+
+	public class RedisService : IRedisService
 	{
+		private readonly IDatabase _redis;
+		private readonly ILogger<RedisService> _logger;
+
+		public RedisService(IConnectionMultiplexer multiplexer, ILogger<RedisService> logger)
+		{
+			_redis = multiplexer.GetDatabase();
+			_logger = logger;
+		}
+
+		public async Task<bool> DeleteCacheAsync(Guid eventId)
+		{
+			try
+			{
+				var cacheKey = GetCacheKeyByEventId(eventId);
+				return await _redis.KeyDeleteAsync(cacheKey);
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError($"При удалении Кэша для eventId = {eventId} произошла ошибка. {ex.Message}");
+			}
+			return false;
+			
+		}
+
+		public async Task<EventModel?> GetCacheForIdAsync(Guid eventId)
+		{
+			try
+			{
+				var cacheKey = GetCacheKeyByEventId(eventId);
+
+				var cached = await _redis.StringGetAsync(cacheKey);
+				if (cached.HasValue && !cached.IsNullOrEmpty)
+					return JsonSerializer.Deserialize<EventModel>(cached!);
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError($"При получении Кэша для eventId = {eventId} произошла ошибка. {ex.Message}");
+			}
+			return null;
+		}
+
+		public async Task<bool> SetCacheAsync(Guid eventId, EventModel value)
+		{
+			try
+			{
+				var cacheKey = GetCacheKeyByEventId(eventId);
+				return await _redis.StringSetAsync(cacheKey, JsonSerializer.Serialize(value), TimeSpan.FromMinutes(10));
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError($"При записи Кэша для eventId = {eventId} произошла ошибка. {ex.Message}");
+			}
+			return false;
+		}
+
+		public async Task<List<EventModel>?> GetTop10EventsAsync()
+		{
+			try
+			{
+				var key = GetCacheKeyByTop10();
+				var cached = await _redis.StringGetAsync(key);
+				if (cached.HasValue && !cached.IsNullOrEmpty)
+				{
+					return JsonSerializer.Deserialize<List<EventModel>>(cached!);
+				}
+			}
+			catch(Exception ex)
+			{
+				_logger.LogError($"При получении Кэша для 10 событий с наибольшим процентом проданных мест произошла ошибка. {ex.Message}");
+			}
+			return null;
+		}
+
+		public async Task<bool> SetTop10EventsAsync(List<EventModel> topEvents)
+		{
+			try
+			{
+				var key = GetCacheKeyByTop10();
+				return await _redis.StringSetAsync(key, JsonSerializer.Serialize(topEvents), TimeSpan.FromMinutes(15));
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError($"При записи Кэша для 10 событий с наибольшим процентом проданных мест произошла ошибка. {ex.Message}");
+			}
+			return false;
+
+		}
+
+		private string GetCacheKeyByEventId(Guid evenId)
+		{
+			return $"event:{evenId}";
+		}
+		private string GetCacheKeyByTop10()
+		{
+			return $"events:top10";
+		}
 	}
+#pragma warning restore CS1591
+
 }
